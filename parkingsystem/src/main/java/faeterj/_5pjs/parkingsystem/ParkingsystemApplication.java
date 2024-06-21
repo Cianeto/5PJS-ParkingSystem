7921b25e1d549f7c88752f464cf6a0edb671dc2d
Cianeto <gbdopu02@gmail.com>
2024-06-21 19:45:17 -0300

    👔Testing insert -> Successful insert

--- parkingsystem/src/main/java/faeterj/_5pjs/parkingsystem/ParkingsystemApplication.java
+++ parkingsystem/src/main/java/faeterj/_5pjs/parkingsystem/ParkingsystemApplication.java
@@ -2,6 +2,7 @@ package faeterj._5pjs.parkingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import faeterj._5pjs.parkingsystem.dto.ClienteDTO;
 import faeterj._5pjs.parkingsystem.service.ClienteService;
@@ -10,9 +11,10 @@ import faeterj._5pjs.parkingsystem.service.ClienteService;
 public class ParkingsystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ParkingsystemApplication.class, args);
+		//SpringApplication.run(ParkingsystemApplication.class, args); // Fiz isso pra conseguir fazer o teste na main
+		ApplicationContext context = SpringApplication.run(ParkingsystemApplication.class, args);
+		ClienteService teste = context.getBean(ClienteService.class);
 
-		ClienteService teste = new ClienteService();
 		ClienteDTO cliente = new ClienteDTO("Zé Maluco", "123.123.123-01", "(21) 98361-4941");
 
 		teste.insertNewCliente(cliente);
