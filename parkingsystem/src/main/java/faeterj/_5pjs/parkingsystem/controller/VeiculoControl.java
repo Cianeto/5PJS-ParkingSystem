cc2d857c8ba3f7ae2646c38b9102149c6e3d1af1
Cianeto <gbdopu02@gmail.com>
2024-06-21 21:20:04 -0300

    🔨 Edited application.properties and dependencies

--- parkingsystem/src/main/java/faeterj/_5pjs/parkingsystem/controller/VeiculoControl.java
+++ parkingsystem/src/main/java/faeterj/_5pjs/parkingsystem/controller/VeiculoControl.java
@@ -1,7 +1,9 @@
 package faeterj._5pjs.parkingsystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @Controller
 public class VeiculoControl {
     
