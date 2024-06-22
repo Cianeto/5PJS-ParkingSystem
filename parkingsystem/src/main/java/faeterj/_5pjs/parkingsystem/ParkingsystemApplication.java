3eba519245c5d4f8b99adc2791021473b9720c47
kaua-sousaa <kauasousa15_rj@hotmail.com>
2024-06-22 17:39:16 -0300

    alteracao clienteControl para aceitar requisicoes

--- parkingsystem/src/main/java/faeterj/_5pjs/parkingsystem/ParkingsystemApplication.java
+++ parkingsystem/src/main/java/faeterj/_5pjs/parkingsystem/ParkingsystemApplication.java
@@ -2,22 +2,11 @@ package faeterj._5pjs.parkingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import faeterj._5pjs.parkingsystem.dto.ClienteDTO;
-import faeterj._5pjs.parkingsystem.service.ClienteService;
 
 @SpringBootApplication
 public class ParkingsystemApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(ParkingsystemApplication.class, args); // Fiz isso pra conseguir fazer o teste na main
-		ApplicationContext context = SpringApplication.run(ParkingsystemApplication.class, args);
-		ClienteService teste = context.getBean(ClienteService.class);
-
-		ClienteDTO cliente = new ClienteDTO("Zé Maluco", "123.123.123-01", "(21) 98361-4941");
-
-		teste.insertNewCliente(cliente);
+		SpringApplication.run(ParkingsystemApplication.class, args);
 	}
-
 }
--- parkingsystem/src/main/java/faeterj/_5pjs/parkingsystem/controller/ClienteControl.java
+++ parkingsystem/src/main/java/faeterj/_5pjs/parkingsystem/controller/ClienteControl.java
@@ -1,10 +1,50 @@
 package faeterj._5pjs.parkingsystem.controller;
 
-import org.springframework.stereotype.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Optional;
+
+import faeterj._5pjs.parkingsystem.model.ClienteModel;
+import faeterj._5pjs.parkingsystem.repository.ClienteRepo;
 
 @RestController
-@Controller
+@RequestMapping("/ClienteModel")
 public class ClienteControl {
     
+    @Autowired
+    private ClienteRepo clienteRepo;
+
+    @GetMapping
+    public List<ClienteModel> listar (){
+        return clienteRepo.findAll();
+    }
+
+    @PostMapping
+    public void incluir(@RequestBody ClienteModel clienteModel){
+        clienteRepo.save(clienteModel);
+    }
+
+    @PutMapping
+    public void alterar(@RequestBody ClienteModel clienteModel){
+        clienteRepo.save(clienteModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable int id){
+        clienteRepo.deleteById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<ClienteModel> ler (@PathVariable int id){
+        return clienteRepo.findById(id);
+    } 
 }
